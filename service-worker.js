2f7abbf47e2623a136343f531490131ead7bc533
HidekiTakazawa <ht0428a30@gmail.com>
2026-05-23 16:55:04 +0900

    画像変更

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // キャッシュするファイルの名前とバージョンを定義
-const CACHE_NAME = 'banbanmenu-app-showcase-v7';
+const CACHE_NAME = 'banbanmenu-app-showcase-v9';
 // キャッシュするファイルのリスト
 const urlsToCache = [
   './', // index.html を示す
